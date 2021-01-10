3ef5783764f7b7d74d9c605b8ce83a1ceb70b1fe
luphord <luphord@protonmail.com>
2021-01-10 21:36:28 +0100

    add simple index page

---
+++ index/cgi-bin/index.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import json
+from urllib.request import urlopen
+
+host = os.environ.get('HOST')
+
+HEAD = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Private Cloud</title>
+  <style type="text/css">
+
+      body {from urllib.request import urlopen
+    </style>
+  </head>
+  <body>
+  <h1>Private Cloud</h1>
+'''
+
+FOOT = '''
+  </body>
+</html>
+'''
+
+print(HEAD)
+
+print('''
+<ul>
+''')
+
+with urlopen(f"http://{host}:8080/api/http/routers") as f:
+    for router in json.load(f):
+        if router["provider"] != "internal" and "reverse-proxy" not in router["name"] and not router["name"].startswith("index@"):
+            name = router["name"].split("@")[0]
+            route = router["rule"].split("`")[1]
+            print(f'<li><a href="//{route}">{name}</a></li>')
+
+
+print('''
+</ul>
+''')
+
+print(FOOT)
