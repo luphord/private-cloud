73651db2755f196c03bd1c8ccc97810069bbee00
luphord <luphord@protonmail.com>
2021-02-09 21:27:32 +0100

    track bill updates and deletions

--- ihatemoney/run_ihatemoney.sh
+++ ihatemoney/run_ihatemoney.sh
@@ -5,7 +5,9 @@ set -e
 mkdir -p /etc/ihatemoney/
 
 SECRET_KEY=`openssl rand -hex 64`
+SQLALCHEMY_DATABASE_URI="sqlite:///$SQLITE_DATABASE"
 
+# store env variables in config file
 cat <<EOF > /etc/ihatemoney/ihatemoney.cfg
 DEBUG = False
 SQLALCHEMY_DATABASE_URI = "$SQLALCHEMY_DATABASE_URI"
@@ -18,4 +20,11 @@ ALLOW_PUBLIC_PROJECT_CREATION = $ALLOW_PUBLIC_PROJECT_CREATION
 ACTIVATE_ADMIN_DASHBOARD = $ACTIVATE_ADMIN_DASHBOARD
 EOF
 
+# trigger database initialization
+ihatemoney --help
+
+# execute crate table + trigger statements for bill tracking
+sqlite3 $SQLITE_DATABASE < /bin/trigger_bill_update.sql
+
+# start dev server (for now; gunicorn would be better)
 ihatemoney runserver --host 0.0.0.0 --port 80
